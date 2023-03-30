bb560e8f298f650da3f67fbf8fa41da99f061cf2
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-03-30 16:04:47 -0600

    add backend

---
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        /*  ActionResult is a class that represents the result of an action method in an ASP.NET MVC or ASP.NET Core application
+         *  An action result is what a controller action returns in response to a browser request. 
+         *  They are derived from the abstract class ActionResult, which is the base class for all types of responses.
+         * 
+         */
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes() {
+            return new List<SuperHero> {
+                new SuperHero {
+                    Name = "Spider Man",
+                    firstName = "Peter",
+                    lastName = "Parker",
+                    place = " New York"
+                }
+            };
+        }
+    }
+}
---
+++ SuperHeroAPI/SuperHeroAPI/SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperHeroAPI
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string place { get; set; }
+    }
+}
